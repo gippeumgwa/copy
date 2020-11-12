93e1f3d75257fec77aedbbbdd2a83c2916ca3cae
eegh9oi5ioghohhiek3J <59039735+eegh9oi5ioghohhiek3J@users.noreply.github.com>
2020-11-12 09:14:01 +0900

    Update topics.py

--- topics.py
+++ topics.py
@@ -4,4 +4,4 @@ import json
 
 argv = [int(x) for x in sys.argv[1:] if x]
 raw = [random.randint(1, 3100) for x in range(15)]
-print(json.dumps(argv or raw))
+print("::set-output name=topics::"+json.dumps(argv or raw))
